a75a888e3e7dbcb397431bde8e8a7185330c2150
automation85 <automationtest85@gmail.com>
2018-11-30 16:41:34 +0000

    update buildTaskFile

--- buildTask.sh
+++ buildTask.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-set GRADLE_USER_HOME='/opt/gradle/gradle-4.5.1/bin'
+export GRADLE_USER_HOME="/opt/gradle/gradle-4.5.1/bin"
+export PATH=${PATH}:${GRADLE_USER_HOME}
 gradle clean test
